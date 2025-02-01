2e576fc1f9f321c2632b31abbe8698ff8e90d475
Edis Leka <edisleka@yahoo.gr>
2025-02-01 15:25:22 +0200

    About page completed

--- app/(root)/(user)/about/page.tsx
+++ app/(root)/(user)/about/page.tsx
@@ -1,6 +1,27 @@
 import Container from '@/components/Container'
 
 const About = () => {
-  return <Container>About</Container>
+  return (
+    <Container className='max-w-6xl lg:px-8 py-12'>
+      <h1 className='text-3xl font-bold mb-6'>About Shopz</h1>
+      <p className='mb-6'>
+        Shopz is a cutting-edge technology company dedicated to providing
+        innovative solutions for modern businesses. Founded in 2020, we've
+        quickly established ourselves as a leader in digital transformation and
+        software development.
+      </p>
+      <p className='mb-6'>
+        Our team of expert developers, designers, and strategists work
+        tirelessly to create custom solutions that help our clients streamline
+        their operations, increase efficiency, and drive growth.
+      </p>
+      <p className='mb-6'>
+        At Tulos, we believe in the power of technology to transform businesses
+        and improve lives. We're committed to staying at the forefront of
+        technological advancements and delivering exceptional value to our
+        clients.
+      </p>
+    </Container>
+  )
 }
 export default About
